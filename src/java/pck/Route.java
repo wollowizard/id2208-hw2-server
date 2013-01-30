d6d4cef640b66defc60ffca468e31102c97cb1a3
Gerard <Gerard@130.229.135.5>
2013-01-30 18:06:46 +0100

    new

--- src/java/pck/Route.java
+++ src/java/pck/Route.java
@@ -10,56 +10,56 @@ import java.util.ArrayList;
  *
  * @author alfredo
  */
-    public class Route {
-    
-       
+public class Route {
+
     public ArrayList<FlightInfo> flightsOfRoute;
-    public String from;
-    public String to;
-    public String id="Route-";
+    public String from="EMPTYARRAYOFFLIGHTS";
+    public String to="EMPTYARRAYOFFLIGHTS";
+    public String id = "Route-EMPTYARRAYOFFLIGHTS";
     public String flightsId = "";
 
     public Route(ArrayList<FlightInfo> flightInfoArray) {
-        this.flightsOfRoute=flightInfoArray;
-        from=this.flightsOfRoute.get(0).flight.from;
-        to=this.flightsOfRoute.get(this.flightsOfRoute.size()-1).flight.to;
-        for(FlightInfo f : flightInfoArray){
-            id+=f.id;
-            flightsId+=f.flight.from;
-            flightsId+=f.flight.to;
+        if (flightInfoArray == null) {
+            flightInfoArray = new ArrayList<FlightInfo>();
+        }
+        this.flightsOfRoute = flightInfoArray;
+        if (!flightsOfRoute.isEmpty()) {
+            from = this.flightsOfRoute.get(0).flight.from;
+            to = this.flightsOfRoute.get(this.flightsOfRoute.size() - 1).flight.to;
+            for (FlightInfo f : flightInfoArray) {
+                id += f.id;
+            }
         }
     }
-    
-    public void add(FlightInfo fi){
+
+    public void add(FlightInfo fi) {
         flightsOfRoute.add(fi);
-        id+=fi.id;
-        flightsId+=fi.flight.from;
-        flightsId+=fi.flight.to;
+        id += fi.id;
+        flightsId += fi.flight.from;
+        flightsId += fi.flight.to;
     }
-    
-    public FlightInfo get(int position){
+
+    public FlightInfo get(int position) {
         return flightsOfRoute.get(position);
     }
-    
-    public int size(){
+
+    public int size() {
         return flightsOfRoute.size();
     }
-    
-    public String getFrom(){ 
+
+    public String getFrom() {
         return flightsOfRoute.get(0).flight.from;
     }
-    
-    public String getTo(){
+
+    public String getTo() {
         int pos = flightsOfRoute.size() - 1;
         return flightsOfRoute.get(pos).flight.to;
     }
-    
-    public String getDate(){
-        String date = flightsOfRoute.get(0).date.getDate()+"/"+
-                flightsOfRoute.get(0).date.getMonth()+"/"+
-                flightsOfRoute.get(0).date.getYear();
+
+    public String getDate() {
+        String date = flightsOfRoute.get(0).date.getDate() + "/"
+                + flightsOfRoute.get(0).date.getMonth() + "/"
+                + flightsOfRoute.get(0).date.getYear();
         return date;
     }
-    
-    
 }
